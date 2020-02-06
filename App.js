f6b871709ae3ca9ed3d4440e7b77269d87ea391a
Renisson Silva <renissonx@gmail.com>
2020-02-06 15:10:50 -0300

    Adicionando botões na home

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Button, Alert, Text, View, ImageBackground } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <ImageBackground source={require('./images/watch.jpeg')} style={styles.container}>
-      
+      <View style={styles.btns}>
+        <Button 
+          title="Cadastrar"
+          color="#FF5F54"
+          onPress={() => Alert.alert('Funcionando :)')}
+        />
+         <Button
+          title="Entrar"
+          color="#FF5F54"
+          onPress={() => Alert.alert('Funcionando tbm :)')}
+        />
+        </View>
       </ImageBackground>
     );
   }
@@ -15,6 +26,10 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     alignItems:'center',
-    justifyContent:'center',
+    justifyContent:'flex-end',
+  },
+  btns:{
+    flexDirection:'row',
+    margin:10,
   },
 });
