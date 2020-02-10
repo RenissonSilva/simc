31ccc4f1c029aefdf15c7acd639a4e257e49eb14
gmatheus66 <gmatheus66@hotmail.com>
2020-02-10 17:14:37 -0300

    Login Familiar

--- screens/RelativeLogin.js
+++ screens/RelativeLogin.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyleSheet, Text, View, TextInput,SubmitButton, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import querystring from 'query-string';
 
 export default class PatientLogin extends Component {
   constructor(props) {
     super(props);
     this.state = {email: ''};
-    this.state = {senha: ''};
+    this.state = {password: ''};
+    this.state = {token: ''};
+    this.state = {data: null};
+    this.state = {dataerror: null};
   }
+
+  signIn = () => {
+    return axios.post('https://apisimc.herokuapp.com/api/relative/login?',querystring.stringify({
+      email: this.state.email,
+      password: this.state.password,
+    }))
+    .then(res => {this.state.dataresponse = res.data  
+      //console.log(res)
+      this.state.token = res.data.token_type + " " + res.data.access_token
+      //console.log(this.state.token);
+      this.next(this.state.token);
+      //this.props.navigation.navigate('PatientHome', this.state.token )
+    })
+    .catch(error => {console.log(error)})
+    
+  };
+
+  next(item){
+    this.props.navigation.navigate('RelativeHome', {token: item} )
+  };
 
   render() {
     return (
@@ -23,14 +48,12 @@ export default class PatientLogin extends Component {
           <TextInput
             secureTextEntry={true}
             style={styles.input}
-            onChangeText={(senha) => this.setState({senha})}
-            value={this.state.senha}
+            onChangeText={(password) => this.setState({password})}
+            value={this.state.password}
           />
           <TouchableOpacity
                style = {styles.submitButton}
-               onPress = {
-                  () => this.login(this.state.email, this.state.senha)
-               }>
+               onPress = {this.signIn}>
                <Text style = {styles.submitText}> Confirmar </Text>
             </TouchableOpacity>
       </View>
