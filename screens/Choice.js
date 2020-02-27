2ea084780ad4676cda91184a2352ee05111f297b
Renisson Silva <renissonx@gmail.com>
2020-02-27 18:26:23 -0300

    Criação do menu de navegação

--- screens/Choice.js
+++ screens/Choice.js
@@ -1,33 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { StyleSheet, Image, TouchableOpacity, Text, View, ImageBackground } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Text,
+  View,
+  ImageBackground,
+} from 'react-native';
 
 export default class App extends React.Component {
   static navigationOptions = {
-      headerTitle:'Entrar',
-      headerStyle: { backgroundColor: '#FF5F54' },
-      headerTintColor: 'white',
-  }
+    headerTitle: 'Entrar',
+    headerStyle: {backgroundColor: '#FF5F54'},
+    headerTintColor: 'white',
+  };
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textExplica}>Selecione seu perfil</Text>
-        <TouchableOpacity style={styles.btn} onPress = {() => { this.props.navigation.navigate('PatientLogin')}}
-            >       
-          <Icon name="user" size={70} color="#fff" style={styles.icon}/>
-          <Text style={styles.btnText}>
-          Paciente</Text>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            this.props.navigation.navigate('PatientLogin');
+          }}>
+          <Icon name="user" size={70} color="#fff" style={styles.icon} />
+          <Text style={styles.btnText}>Paciente</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn} onPress = {() => { this.props.navigation.navigate('RelativeLogin')}}
-            >
-        <Icon name="users" size={70} color="#fff" style={styles.icon}/>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            this.props.navigation.navigate('RelativeLogin');
+          }}>
+          <Icon name="users" size={70} color="#fff" style={styles.icon} />
           <Text style={styles.btnText}>Familiar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.btn} onPress = {() => { this.props.navigation.navigate('DoctorLogin')}}
-            >
-        <Icon name="user-md" size={70} color="#fff" style={styles.icon}/>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => {
+            this.props.navigation.navigate('DoctorLogin');
+          }}>
+          <Icon name="user-md" size={70} color="#fff" style={styles.icon} />
           <Text style={styles.btnText}>Médico</Text>
         </TouchableOpacity>
       </View>
@@ -36,55 +51,55 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  topMenu:{
-    backgroundColor:'#FF5F54',
-    height:70,
-    flexDirection:'row',
-  },
-  textMenu:{
-    flex:2,
-    color:'#fff',
-    fontSize:24,
-    alignSelf:'center',
-  },
-  textExplica:{
-    color:'#FF5F54',
-    fontSize:26,
-    alignSelf:'center',
-    marginTop:30,
-    marginBottom:30,
-  },
-  btn:{
+  topMenu: {
+    backgroundColor: '#FF5F54',
+    height: 70,
     flexDirection: 'row',
-    justifyContent:'center',
-    width:"75%",
+  },
+  textMenu: {
+    flex: 2,
+    color: '#fff',
+    fontSize: 24,
+    alignSelf: 'center',
+  },
+  textExplica: {
+    color: '#FF5F54',
+    fontSize: 26,
+    alignSelf: 'center',
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  btn: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '75%',
     height: 110,
-    backgroundColor:'#FF5F54',
-    margin:15,
-    borderRadius:10,
-    alignSelf:'center',
+    backgroundColor: '#FF5F54',
+    margin: 15,
+    borderRadius: 10,
+    alignSelf: 'center',
   },
-  btnText:{
-    alignSelf:'center',
-    color:'#fff',
-    fontSize:26,
+  btnText: {
+    alignSelf: 'center',
+    color: '#fff',
+    fontSize: 26,
   },
-  imgLogo:{
+  imgLogo: {
     width: 90,
     height: 90,
-    margin:10,
-    alignSelf:'center',
+    margin: 10,
+    alignSelf: 'center',
   },
-  icon:{
-    alignSelf:'center',
-    marginRight:'5%',
+  icon: {
+    alignSelf: 'center',
+    marginRight: '5%',
   },
-  iconeSeta:{
-    alignSelf:'center',
-    marginRight:'10%',
-    marginLeft:'5%',
-  }
+  iconeSeta: {
+    alignSelf: 'center',
+    marginRight: '10%',
+    marginLeft: '5%',
+  },
 });
--- screens/Patient/PatientHome.js
+++ screens/Patient/PatientHome.js
@@ -1,115 +1,118 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import {StyleSheet, TouchableOpacity, Button, View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import * as Animatable from 'react-native-animatable';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { StyleSheet, Image, TouchableOpacity, Text, View, ImageBackground } from 'react-native';
-import axios from 'axios';
-import querystring from 'query-string';
- 
-export default class PatientHome extends React.Component {
-  static navigationOptions = {
-      headerTitle:'Paciente',
-      headerStyle: { backgroundColor: '#FF5F54' },
-      headerTintColor: 'white',
-  }
 
-    constructor(props){
-        super(props)
-        this.state = {data: null};
-        this.state = {dataerror: null};
-    }
-    componentDidMount() {
-        const { params } = this.props.navigation.state;
-        console.log(params.token);
+const Stack = createMaterialTopTabNavigator();
 
-        if(params){
-            axios.defaults.headers.common.accept = 'application/json';            
-
-            axios.get('https://apisimc.herokuapp.com/api/patient/detail', { 
-                headers: {"Authorization": params.token}
-            }
-            )
-            .then( res => { 
-                this.state.data =  res.data;
-                console.log(this.state.data)
-            })
-            .catch( error => { 
-                this.state.dataerror = error
-                console.log(this.state.dataerror)
-                if(this.state.dataerror){
-                    this.props.navigation.goBack();
-                }
-             })
-        }
-    }
-
-    render() {
-        return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.btn}>       
-              <Text style={styles.btnText}>Batimento cardíaco</Text>
-              <View style={styles.teste}>
-                <Text style={styles.btnBpm}>81</Text>
-                <Text style={styles.btnB}>bpm</Text>
-              </View>
-              <Icon name="heartbeat" size={70} color="#FF5F54" style={styles.icon}/>
-              <Animatable.Text animation='pulse' iterationCount={'infinite'} direction="alternate" style={styles.icon}><Icon name="heartbeat" size={70} color="#FF5F54" style={styles.icon}/></Animatable.Text>
-            </TouchableOpacity>
-         <Text style={styles.monitora}>Monitoramento dos últimos 30 minutos</Text>
-      </View>
-        );
-    }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        tabBarOptions={{
+          activeTintColor: '#FF5F54',
+          inactiveTintColor: '#CCCCCC',
+          labelStyle: {
+            fontSize: 13,
+          },
+          indicatorStyle: {
+            backgroundColor: '#FF5F54',
+          },
+        }}>
+        <Stack.Screen name="Status" component={HomeScreen} />
+        <Stack.Screen name="Chat" component={DetailsScreen} />
+        <Stack.Screen name="Perfil" component={DetailsScreen} />
+        <Stack.Screen name="Familiares" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
+function HomeScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Batimento cardíaco</Text>
+        <View style={styles.teste}>
+          <Text style={styles.btnBpm}>81</Text>
+          <Text style={styles.btnB}>bpm</Text>
+        </View>
+        <Icon name="heartbeat" size={70} color="#FF5F54" style={styles.icon} />
+        <Animatable.Text
+          animation="pulse"
+          iterationCount={'infinite'}
+          direction="alternate"
+          style={styles.icon}>
+          <Icon
+            name="heartbeat"
+            size={70}
+            color="#FF5F54"
+            style={styles.icon}
+          />
+        </Animatable.Text>
+      </TouchableOpacity>
+      <Text style={styles.monitora}>Monitoramento dos últimos 30 minutos</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  teste:{
-    flexDirection:'row',
+  teste: {
+    flexDirection: 'row',
   },
-  btn:{
-    justifyContent:'center',
-    width:"75%",
+  btn: {
+    justifyContent: 'center',
+    width: '75%',
     height: 110,
-    backgroundColor:'#fff',
-    margin:15,
-    borderRadius:10,
+    backgroundColor: '#fff',
+    margin: 15,
+    borderRadius: 10,
     borderWidth: 1,
     borderColor: '#FF5F54',
-    alignSelf:'center',
+    alignSelf: 'center',
   },
-  btnText:{
-    marginLeft:30,
-    color:'#FF5F54',
-    fontSize:16,
+  btnText: {
+    marginLeft: 30,
+    color: '#FF5F54',
+    fontSize: 16,
   },
-  btnBpm:{
-    marginLeft:50,
-    color:'#FF5F54',
-    fontSize:40,
-    fontWeight:'bold',
+  btnBpm: {
+    marginLeft: 50,
+    color: '#FF5F54',
+    fontSize: 40,
+    fontWeight: 'bold',
   },
-  icon:{
-    alignSelf:'center',
-    marginRight:'15%',
-    alignSelf:'flex-end',
-    position:'absolute',
+  icon: {
+    alignSelf: 'center',
+    marginRight: '15%',
+    alignSelf: 'flex-end',
+    position: 'absolute',
   },
-  monitora:{
-    color:'#8B8B8B',
-    fontSize:16,
-    alignSelf:'center',
+  monitora: {
+    color: '#8B8B8B',
+    fontSize: 16,
+    alignSelf: 'center',
   },
-  btnB:{
-    color:'#FF5F54',
-    fontSize:21,
-    alignSelf:'flex-end',
-    marginLeft:5,
+  btnB: {
+    color: '#FF5F54',
+    fontSize: 21,
+    alignSelf: 'flex-end',
+    marginLeft: 5,
   },
-
 });
-        // <View style={styles.topMenu}>
-        //   <Icon name="arrow-left" size={40} color="#fff" style={styles.iconeSeta}/>
-        //   <Text style={styles.textMenu}>Login</Text>
-        //   <Image style={styles.imgLogo} source={require('../images/whiteLogo.png')} />
-        // </View> 
