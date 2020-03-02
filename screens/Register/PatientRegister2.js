284690f8fd8ca90ebdaaf1a60dec53491fb4dece
gmatheus66 <gmatheus66@hotmail.com>
2020-03-02 05:04:59 -0300

    modificando e adicionando validação para o cadastro do paciente part.2

--- screens/Register/PatientRegister2.js
+++ screens/Register/PatientRegister2.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity,ScrollView } from 'react-native';
 import axios from 'axios';
 import querystring from 'query-string';
 import moment from 'moment';
 import { TextInputMask } from 'react-native-masked-text';
+import {Formik, useFormikContext} from 'formik';
+import * as yup from 'yup';
 
 export default class PatientRegister2 extends Component {
 
@@ -27,8 +29,8 @@ static navigationOptions = {
 
   }
   componentDidMount(){
-    //const { params } = this.props.navigation.state;
-    //console.log(params.idade);
+    const {params}= this.props.navigation.state;
+    console.log(params);
     moment.locale('pt-BR');
     //console.log(moment(params.idade).isValid());
     //console.log(moment(params.idade,['DD-MM-YYYY','YYYY-MM-DD']).utc().format('YYYY-MM-DD'));
@@ -38,66 +40,149 @@ static navigationOptions = {
   render() {
     return (
         <ScrollView style={styles.scrollView}>
-          <Text style={styles.textInput}>CEP</Text>
-          <TextInputMask
-            type={'zip-code'}
-            style={styles.input}
-            onChangeText={(cep) => {
-              this.setState({cep})
-            }}
-            value={this.state.cep}
-            onBlur= {this.procurar_cep}
-            maxLength = {9}
-            ref={(ref) => {this.cepField = ref}}
-          />
-          <Text style={styles.textInput}>Rua</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(rua) => this.setState({rua})}
-            value={this.state.rua}
-          />
-          <Text style={styles.textInput}>País</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(pais) => this.setState({pais})}
-            value={this.state.pais}
-          />
-          <Text style={styles.textInput}>Estado</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(estado) => this.setState({estado})}
-            value={this.state.estado}
-          />
-          <Text style={styles.textInput}>Cidade</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(cidade) => this.setState({cidade})}
-            value={this.state.cidade}
-          />
-          <Text style={styles.textInput}>Número</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={(numero) => this.setState({numero})}
-            value={this.state.numero}
-          />
+          <Formik
+            initialValues={{cep: '', rua: '', cidade: '', estado: '', pais: '', numero: ''}}
+            onSubmit={ values => { this.Register(values)}}
+            validationSchema={yup.object().shape({
+              cep: yup
+              .string()
+              .required('Cep e um campo obrigatório'),
 
-          <TouchableOpacity
-               style = {styles.submitButton}
-               onPress = {this.Register}>
-               <Text style = {styles.submitText}> Confirmar </Text>
-            </TouchableOpacity>
-          
+              rua: yup
+              .string()
+              .required('Rua e um campo obrigatório')
+              .min(5, 'O Campo tem que ter mais de 5 caracteres')
+              .max(25, 'O Campo não pode passar de 25 caracteres'),
+
+              pais: yup
+              .string()
+              .required('Pais e um campo obrigatório')
+              .min(2, 'O Campo tem que ter mais de 2 caracteres')
+              .max(10, 'O Campo não pode passar de 10 caracteres'),
+
+              estado: yup
+              .string()
+              .required('Estado e um campo obrigatório')
+              .min(2, 'O Campo tem que ter mais de 2 caracteres')
+              .max(18, 'O Campo não pode passar de 10 caracteres'),
+
+              cidade: yup
+              .string()
+              .required('Cidade e um campo obrigatório')
+              .min(4, 'O Campo tem que ter mais de 4 caracteres')
+              .max(25, 'O Campo não pode passar de 25 caracteres'),
+              
+              numero: yup
+              .string()
+              .matches(/^[0-9]/,'Este Campo não pode conter Letras')
+              .min(1, 'O Campo tem que ter mais de 1 números')
+              .max(5, 'O Campo não pode passar de 5 caracteres')
+              .required('Numero e um campo obrigatório'),
+
+
+            })}
+          >
+        {({ values, handleChange, errors, setFieldTouched, touched, isValid, handleSubmit }) => (
+            <Fragment>
+              <Text style={styles.textInput}>CEP</Text>
+              <TextInputMask
+                type={'zip-code'}
+                style={styles.input}
+                onChangeText={handleChange('cep')}
+                value={values.cep}
+                onBlur= {() => {setFieldTouched('cep'); this.procurar_cep(values.cep) } }
+                maxLength={9}
+                ref={(ref) => {this.cepField = ref}}
+                placeholder='Ex. 55222-100'
+              />
+              { touched.cep && errors.cep &&
+                <Text style={{fontSize:10, color: 'red'}}>{errors.cep}</Text>
+              }
+
+              <Text style={styles.textInput}>Rua</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={ handleChange('rua') }
+                value={values.rua}
+                onBlur={() => { setFieldTouched('rua')}}
+                placeholder='Ex. Av. fim do Mundo'
+              />
+              { touched.rua && errors.rua &&
+                <Text style={{fontSize:10, color: 'red'}}>{errors.rua}</Text>
+              }
+
+              <Text style={styles.textInput}>País</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange('pais')}
+                value={values.pais}
+                onBlur={()=>setFieldTouched('pais')}
+                placeholder='Ex. Brazil'
+              />
+              { touched.pais && errors.pais &&
+                <Text style={{fontSize:10, color: 'red'}}>{errors.pais}</Text>
+              }
+
+
+              <Text style={styles.textInput}>Estado</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange('estado')}
+                value={values.estado}
+                onBlur={() => setFieldTouched('estado')}
+                placeholder='Ex.  Acre'
+              />
+              { touched.estado && errors.estado &&
+                <Text style={{fontSize:10, color: 'red'}}>{errors.estado}</Text>
+              }
+
+              <Text style={styles.textInput}>Cidade</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange('cidade')}
+                value={values.cidade}
+                onBlur={()=>setFieldTouched('cidade')}
+                placeholder='Ex. Hellcife'
+              />
+              { touched.cidade && errors.cidade &&
+                <Text style={{fontSize:10, color: 'red'}}>{errors.cidade}</Text>
+              }
+
+              <Text style={styles.textInput}>Número</Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange('numero')}
+                value={values.numero}
+                onBlur={() => setFieldTouched('numero')}
+                placeholder='Ex. 404'
+              />
+              { touched.numero && errors.numero &&
+                <Text style={{fontSize:10, color: 'red'}}>{errors.numero}</Text>
+              }
+
+              <TouchableOpacity
+                  style = {styles.submitButton}
+                  disabled={!isValid}
+                  onPress = {handleSubmit}>
+                  <Text style = {styles.submitText}> Confirmar </Text>
+                </TouchableOpacity>
+            </Fragment>
+            )}
+          </Formik>
         </ScrollView>
+
 
     );
   }
 
-  procurar_cep = () => {
-    if(this.state.cep){
-      axios.get('https://viacep.com.br/ws/'+this.state.cep+'/json')
+  procurar_cep(cep){
+    //console.log(cep)
+    if(cep){
+      axios.get('https://viacep.com.br/ws/'+cep+'/json')
       .then( response => {
         //console.log(response.data)
         if(!response.data.error){
+          
           this.setState({rua: response.data.logradouro})
           this.setState({cidade: response.data.localidade})
           this.setState({estado: response.data.uf})
@@ -110,31 +195,30 @@ static navigationOptions = {
     }
   }
 
-  Register = () => {
+  Register(values){
     const { params } = this.props.navigation.state;
     //console.log(params);
-    console.log(this.state)
+    //console.log(this.state)
     let cep = this.cepField.getRawValue()
     console.log(cep);
     this.setState({cepunmasked: cep});
-    console.log( moment(params.idade,['DD-MM-YYYY','YYYY-MM-DD']).utc().format('YYYY-MM-DD'));
+    console.log( moment(params.values.idade,['DD-MM-YYYY','YYYY-MM-DD']).utc().format('YYYY-MM-DD'));
     //console.log(this.state.cepunmasked);
-    
     if(this.state.cepunmasked){
       if(params){
           axios.post('https://apisimc.herokuapp.com/api/patient/register?', querystring.stringify({
-          name: params.nome,
-          sex: params.sexo,
+          name: params.values.nome,
+          sex: params.values.sexo,
           telephone: params.telefoneunmasked,
-          occupation: params.ocupacao,
-          address: this.state.rua,
-          city: this.state.cidade,
-          country: this.state.estado,
-          state_province: this.state.pais,
+          occupation: params.values.ocupacao,
+          address: values.rua,
+          city: values.cidade,
+          country: values.estado,
+          state_province: values.pais,
           zip: this.state.cepunmasked,
-          password: params.senha,
-          birthday: moment(params.idade,['DD-MM-YYYY','YYYY-MM-DD']).utc().format('YYYY-MM-DD'),
-          email: params.email
+          password: params.values.senha,
+          birthday: moment(params.values.idade,['DD-MM-YYYY','YYYY-MM-DD']).utc().format('YYYY-MM-DD'),
+          email: params.values.email
         }))
         .then(response => {
           console.log(response.data)
