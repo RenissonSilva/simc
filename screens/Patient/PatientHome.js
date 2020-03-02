87e8a5de78912aa9778fe20cfcb3c1afa2212e41
gmatheus66 <gmatheus66@hotmail.com>
2020-03-02 19:45:27 -0300

    Mudando componentes para ficarem reutilizáveis

--- screens/Patient/PatientHome.js
+++ screens/Patient/PatientHome.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   TouchableOpacity,
@@ -11,151 +11,38 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import * as Animatable from 'react-native-animatable';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import HomeScreen from '../HomeComponent/HomeScreen';
+import ChatScreen from '../HomeComponent/ChatSreen';
+import ProfileScreen from '../HomeComponent/ProfileScreen';
+import RelativeScreen from '../HomeComponent/RelativeScreen';
+
 
 const Stack = createMaterialTopTabNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        tabBarOptions={{
-          activeTintColor: '#FF5F54',
-          inactiveTintColor: '#CCCCCC',
-          labelStyle: {
-            fontSize: 13,
-          },
-          indicatorStyle: {
-            backgroundColor: '#FF5F54',
-          },
-        }}>
-        <Stack.Screen name="Status" component={HomeScreen} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
-        <Stack.Screen name="Perfil" component={ProfileScreen} />
-        <Stack.Screen name="Familiares" component={RelativeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class PatientHome extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          tabBarOptions={{
+            activeTintColor: '#FF5F54',
+            inactiveTintColor: '#CCCCCC',
+            labelStyle: {
+              fontSize: 13,
+            },
+            indicatorStyle: {
+              backgroundColor: '#FF5F54',
+            },
+          }}>
+          <Stack.Screen name="Status" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+          <Stack.Screen name="Perfil" component={ProfileScreen} />
+          <Stack.Screen name="Familiares" component={RelativeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.btn}>
-        <Text style={styles.btnText}>Batimento cardíaco</Text>
-        <View style={styles.teste}>
-          <Text style={styles.btnBpm}>81</Text>
-          <Text style={styles.btnB}>bpm</Text>
-        </View>
-        <Icon name="heartbeat" size={70} color="#FF5F54" style={styles.icon} />
-        <Animatable.Text
-          animation="pulse"
-          iterationCount={'infinite'}
-          direction="alternate"
-          style={styles.icon}>
-          <Icon
-            name="heartbeat"
-            size={70}
-            color="#FF5F54"
-            style={styles.icon}
-          />
-        </Animatable.Text>
-      </TouchableOpacity>
-      <Text style={styles.monitora}>Monitoramento dos últimos 30 minutos</Text>
-    </View>
-  );
-}
-
-function ProfileScreen() {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.imgProfile}
-        source={require('../../images/profilePatient.jpg')}
-      />
-      <Text style={styles.nome}>Renato Silva</Text>
-      <Text style={styles.ano}>1996</Text>
-
-      <View style={styles.containerDados}>
-        <View style={styles.infoIcon}>
-          <Image
-            style={styles.imgIcon}
-            source={require('../../images/info.png')}
-          />
-          <Text style={styles.info}>Informações pessoais</Text>
-        </View>
-        <Text style={styles.dado}>Telefone : 988445577</Text>
-        <Text style={styles.dado}>Sexo : Masculino</Text>
-        <Text style={styles.dado}>E-mail: renatosilva@gmail.com</Text>
-        <Text style={styles.dado}>Ocupação: Engenheiro</Text>
-
-        <View style={styles.infoIcon}>
-          <Image
-            style={styles.imgIcon}
-            source={require('../../images/address.png')}
-          />
-          <Text style={styles.info}>Endereço</Text>
-        </View>
-        <Text style={styles.dado}>Av. Fagundes Varela, 100</Text>
-        <Text style={styles.dado}>Cidade: Olinda </Text>
-        <Text style={styles.dado}>Estado: Pernambuco</Text>
-        <Text style={styles.dado}>CEP: 21652-100</Text>
-      </View>
-    </View>
-  );
-}
-
-function ChatScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>CHAT!</Text>
-    </View>
-  );
-}
-
-function RelativeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Familiares cadastrados</Text>
-      <Image style={styles.sadIcon} source={require('../../images/sad.png')} />
-      <Text style={styles.msgEmpty}>No momento não há</Text>
-      <Text style={styles.title}>Convites</Text>
-
-      <View style={styles.boxFamiliar}>
-        <Image
-          style={styles.imgProfileRelative}
-          source={require('../../images/profileRelative.jpg')}
-        />
-        <View style={styles.boxFamiliarText}>
-          <Text style={styles.msg}>Júlia é sua irmã ?</Text>
-          <View style={styles.buttons}>
-            <TouchableOpacity
-              style={styles.check}
-              onPress={() => {
-                this.props.navigation.navigate('PatientLogin');
-              }}>
-              <Image
-                style={styles.imgAccept}
-                source={require('../../images/accept.png')}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.check}
-              onPress={() => {
-                this.props.navigation.navigate('PatientLogin');
-              }}>
-              <Image
-                style={styles.imgDenied}
-                source={require('../../images/denied.png')}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
-}
-export default App;
 
 const styles = StyleSheet.create({
   container: {
