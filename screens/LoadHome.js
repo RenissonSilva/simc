bd37cea49a3682a4744af4ad0fa7290602723be9
gmatheus66 <gmatheus66@hotmail.com>
2020-03-13 17:33:52 -0300

    Verificando internet para ser possivel o redirecionamento se ja estiver logado

--- screens/LoadHome.js
+++ screens/LoadHome.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import LottieView from 'lottie-react-native';
-// import { Container } from './styles';
+import NetInfo from "@react-native-community/netinfo";
 
 export default class LoadHome extends Component {
 
@@ -11,20 +11,42 @@ export default class LoadHome extends Component {
         headerShown: false
     }
 
+    constructor(props){
+        super(props);
+        this.state = {isConnected: false}
+    }
     
     componentDidMount(){
+        NetInfo.fetch().then(state => {
+            state.isConnected ? this.setState({isConnected: state.isConnected}) : console.log("Is connected?", this.state.isConnected);
+        });
+
         AsyncStorage.getItem('Token').then( evt => {
-             if(evt == null){
+             if(evt == null || !this.state.isConnected){
                 this.props.navigation.navigate('Home')
              }
              else{
                 this.props.navigation.navigate('PatientHome')
-             }
+            }
         })
+        
     }
     render() {
         return (
-            <LottieView source={require('../27-loading.json')} autoPlay loop />
+            <View style={styles.background}>
+                <LottieView source={require('../27-loading.json')} autoPlay loop />
+            </View>
         );
     }
+
+
+
 }
+
+
+const styles = StyleSheet.create({
+    background:{
+        backgroundColor:'#FF5F54',
+    }
+
+})
