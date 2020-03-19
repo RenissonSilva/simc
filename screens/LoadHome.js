6cf88d79d8ebaef22a51ee12f6eddc50ce4adf02
gmatheus66 <gmatheus66@hotmail.com>
2020-03-18 23:12:11 -0300

    Modificações na verificação do launcher e alteração na animação

--- screens/LoadHome.js
+++ screens/LoadHome.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, Platform} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import LottieView from 'lottie-react-native';
 import NetInfo from "@react-native-community/netinfo";
-
+  
 export default class LoadHome extends Component {
 
     static navigationOptions = {
@@ -13,29 +13,38 @@ export default class LoadHome extends Component {
 
     constructor(props){
         super(props);
-        this.state = {isConnected: false}
+        this.state = {isConnected: false, token: '', user: ''}
     }
     
     componentDidMount(){
+        this.animation.play();
         NetInfo.fetch().then(state => {
             state.isConnected ? this.setState({isConnected: state.isConnected}) : console.log("Is connected?", this.state.isConnected);
         });
 
-        AsyncStorage.getItem('Token').then( evt => {
+        AsyncStorage.multiGet(['Token','User']).then( evt => {
+
+            (evt[0][1] && evt[1][1] && this.state.isConnected ) ? this.props.navigation.navigate('PatientHome') : this.props.navigation.navigate('Home')
+            //console.log(evt[0][1],evt[1][1]);
              if(evt == null || !this.state.isConnected){
-                this.props.navigation.navigate('Home')
+                //this.props.navigation.navigate('Home')
+                
              }
              else{
-                this.props.navigation.navigate('PatientHome')
+                //this.props.navigation.navigate('PatientHome')
             }
         })
+    }
+    checkToken(user, token){
         
     }
     render() {
         return (
-            <View style={styles.background}>
-                <LottieView source={require('../27-loading.json')} autoPlay loop />
-            </View>
+           <LottieView ref={animation => {
+                this.animation = animation;
+            }}
+            source={require('../lf30_editor_iob6yv.json')}
+            /> 
         );
     }
 
