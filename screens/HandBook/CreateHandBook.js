4c49591fe0f6c3bd784e5bc1c40267b6e9d39282
gmatheus66 <gmatheus66@hotmail.com>
2020-04-22 18:54:35 -0300

    Parte da estilizacao da criacao do prontuario

--- screens/HandBook/CreateHandBook.js
+++ screens/HandBook/CreateHandBook.js
@@ -1,9 +1,12 @@
 import React, {Fragment} from 'react';
-import { View,ScrollView, Picker, Text, TextInput,   TouchableOpacity} from 'react-native';
+import { View,ScrollView, Text, TextInput, TouchableOpacity, Picker} from 'react-native';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+//import {Picker} from '@react-native-community/picker';
 
-// import { Container } from './styles';
+import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
+
+import styles from './style';
 
 export default function HandBook() {
   return (
@@ -83,9 +86,11 @@ export default function HandBook() {
         })}>
 
         {({values,handleChange, errors , setFieldTouched, touched, isValid, handleSubmit}) => (
-            <Fragment>
-                <Text>Name</Text>
+        <ScrollView style={styles.scrollView}>
+           <Fragment>
+                <Text style={styles.textInput}>Nome</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('nome')}
                     value={values.nome}
                     onBlur={() => setFieldTouched('nome')}
@@ -93,125 +98,144 @@ export default function HandBook() {
                 />
                 <View>
                     {touched.nome && errors.nome && (
-                        <Text>{errors.nome}</Text>
+                        <Text style={styles.validationError}>{errors.nome}</Text>
                     ) }
                 </View>
-                <Text>Limitaçoes</Text>
+                <Text style={styles.textInput}>Limitaçoes</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('limitacoes')}
                     value={values.limitacoes}
                     onBlur={() => setFieldTouched('limitacoes')}
                 />
                 <View>
                     {touched.limitacoes && errors.limitacoes && (
-                        <Text>{errors.limitacoes}</Text>
+                        <Text style={styles.validationError}>{errors.limitacoes}</Text>
                     ) }
                 </View>
-                <Text>Massa Corporal</Text>
+                <Text style={styles.textInput}>Massa Corporal</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('massa_corporal')}
                     value={values.massa_corporal}
                     onBlur={() => setFieldTouched('massa_corporal')}
                 />
                 <View>
                     {touched.massa_corporal && errors.massa_corporal && (
-                        <Text>{errors.massa_corporal}</Text>
+                        <Text style={styles.validationError}>{errors.massa_corporal}</Text>
                     ) }
                 </View>
-                <Text>Peso</Text>
+                <Text style={styles.textInput}>Peso</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('peso')}
                     value={values.peso}
                     onBlur={() => setFieldTouched('peso')}
                 />
                 <View>
                     {touched.peso && errors.peso && (
-                        <Text>{errors.peso}</Text>
-                    ) }
-                </View>
-                <Text>Peso</Text>
-                <TextInput
-                    onChangeText={handleChange('peso')}
-                    value={values.peso}
-                    onBlur={() => setFieldTouched('peso')}
-                />
-                <View>
-                    {touched.peso && errors.peso && (
-                        <Text>{errors.peso}</Text>
+                        <Text style={styles.validationError}>{errors.peso}</Text>
                     ) }
                 </View>
 
-                <Text>Date</Text>
 
-                <Text>Reclamacoes</Text>
-                <TextInput
+                <Text style={styles.textInput}>Reclamacoes</Text>
+                <AutoGrowingTextInput
+                    style={styles.inputArea}
                     onChangeText={handleChange('reclamacoes')}
                     value={values.reclamacoes}
                     onBlur={() => setFieldTouched('reclamacoes')}
+                    maxHeight={200}
+                    minHeight={45}
+                    
                 />
                 <View>
                     {touched.reclamacoes && errors.reclamacoes && (
-                        <Text>{errors.reclamacoes}</Text>
+                        <Text style={styles.validationError}>{errors.reclamacoes}</Text>
                     ) }
                 </View>
 
-                <Text>Sintomas</Text>
-                <TextInput
+                <Text style={styles.textInput}>Sintomas</Text>
+                <AutoGrowingTextInput
+                    style={styles.input}
                     onChangeText={handleChange('sintomas')}
                     value={values.sintomas}
                     onBlur={() => setFieldTouched('sintomas')}
+                    maxHeight={200}
+                    minHeight={45}
                 />
                 <View>
                     {touched.sintomas && errors.sintomas && (
-                        <Text>{errors.sintomas}</Text>
+                        <Text style={styles.validationError}>{errors.sintomas}</Text>
                     ) }
                 </View>
 
-                <Text>Sinais Vitais</Text>
+                <Text style={styles.textInput}>Sinais Vitais</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('sinais_vitais')}
                     value={values.sinais_vitais}
                     onBlur={() => setFieldTouched('sinais_vitais')}
                 />
                 <View>
                     {touched.sinais_vitais && errors.sinais_vitais && (
-                        <Text>{errors.sinais_vitais}</Text>
+                        <Text style={styles.validationError}>{errors.sinais_vitais}</Text>
                     )}
                 </View>
 
-                <Text>Tipo Sanguineo</Text>
+                <Text style={styles.textInput}>Tipo Sanguineo</Text>
+                <Picker
+                    selectedValue={values.tipo_sanguineo}
+                    style={styles.picker}
+                    onValueChange={handleChange('tipo_sanguineo')}
+                    onBlur={() => setFieldTouched('tipo_sanguineo')} >
+                    <Picker.Item label="Escolha uma opção" value=""/>
+                    <Picker.Item label="A+" value="A+"/>
+                    <Picker.Item label="A-" value="A-"/>
+                    <Picker.Item label="B+" value="B+"/>
+                    <Picker.Item label="B-" value="B-"/>
+                    <Picker.Item label="AB+" value="AB+"/>
+                    <Picker.Item label="AB-" value="AB-"/>
+                    <Picker.Item label="O+" value="O+"/>
+                    <Picker.Item label="O-" value="O-"/>
+                </Picker>
                     
-                <Text>Pressao Sanguinea</Text>
+                <Text style={styles.textInput}>Pressao Sanguinea</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('pressao_sanguinea')}
                     value={values.pressao_sanguinea}
                     onBlur={() => setFieldTouched('pressao_sanguinea')}
                 />
                 <View>
                     {touched.pressao_sanguinea && errors.pressao_sanguinea && (
-                        <Text>{errors.pressao_sanguinea}</Text>
+                        <Text style={styles.validationError}>{errors.pressao_sanguinea}</Text>
                     )}
                 </View>
                     
-                <Text>HGT (Nível Glicemia)</Text>
+                <Text style={styles.textInput}>HGT (Nível Glicemia)</Text>
                 <TextInput
+                    style={styles.input}
                     onChangeText={handleChange('hgt')}
                     value={values.hgt}
                     onBlur={() => setFieldTouched('hgt')}
                 />
                 <View>
                     {touched.hgt && errors.hgt && (
-                        <Text>{errors.hgt}</Text>
+                        <Text style={styles.validationError}>{errors.hgt}</Text>
                     )}
                 </View>
 
                 <TouchableOpacity
-                disabled={!isValid}
-                onPress={handleSubmit}>
-                    <Text> Registrar </Text>
+                    style={styles.submitButton}
+                    disabled={!isValid}
+                    onPress={handleSubmit}>
+                    <Text style={styles.submitText}> Registrar </Text>
                 </TouchableOpacity>
 
             </Fragment>
+        </ScrollView>
+        
         )}
     </Formik>
   );
