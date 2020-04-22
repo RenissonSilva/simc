96f33cac3ae05e325bad63fcf5e89646d7b63c18
gmatheus66 <gmatheus66@hotmail.com>
2020-04-22 18:57:58 -0300

    Estilo da pagina de criacao do prontuario

---
+++ screens/HandBook/style.js
@@ -0,0 +1,60 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    textInput: {
+        color: '#FF5F54',
+        fontSize: 20,
+        marginTop: 20,
+        marginBottom: 5,
+        marginLeft: 60,
+    },
+    input: {
+        height: 40,
+        marginLeft: 60,
+        marginRight: 60,
+        borderBottomColor: '#FF5F54',
+        borderBottomWidth: 3,
+    },
+    submitButton: {
+        backgroundColor: '#FF5F54',
+        alignSelf: 'center',
+        marginTop: 50,
+        height: 50,
+        width: '45%',
+        justifyContent: 'center',
+    },
+    scrollView: {
+        marginBottom: 50,
+    },
+    picker: {
+        color: '#FF5F54',
+        marginLeft: 50,
+        marginRight: 50,
+    },
+    validationError: {
+        marginTop: 20,
+        fontSize: 15,
+        color: '#e35046',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+    },
+    submitText: {
+        color: '#fff',
+        fontSize: 21,
+        alignSelf: 'center',
+    },
+    inputArea: {
+        height: 90,
+        marginLeft: 60,
+        marginRight: 60,
+        borderBottomColor: '#FF5F54',
+        borderBottomWidth: 3,
+    },
+    picker: {
+        color: '#FF5F54',
+        marginLeft: 50,
+        marginRight: 50,
+    },
+
+
+})
