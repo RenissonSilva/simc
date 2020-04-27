77292b6538a497668aa2da7af8affb94d68b97b4
gmatheus66 <gmatheus66@hotmail.com>
2020-04-27 19:46:21 -0300

    Modificando barra de pesquisa

--- screens/HomeComponent/SearchUser.js
+++ screens/HomeComponent/SearchUser.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, TextInput, FlatList, ActivityIndicator } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import http from '../../services/axiosconf';
 import { ListItem, SearchBar } from 'react-native-elements';
@@ -13,7 +13,7 @@ export default function SearchUser(props) {
     const [user, setUser ] = useState(true);
     const [token, setToken ] = useState(true);
     const [ patients, setPatients] = useState(0);
-    const [ value, setValue] = useState(true);
+    const [ valueInput, setValueInput] = useState(true);
 
     async function getPatients(user, token) {
         await http.get('/' + user + '/patient',{
@@ -43,18 +43,6 @@ export default function SearchUser(props) {
         getPatients(user,token);
     });
 
-    function SearchHeader() {
-        return(
-            <SearchBar
-                placeholder="Type Here..."
-                lightTheme
-                round
-                onChangeText={text => setValue(text)}
-                autoCorrect={false}
-                value={value}
-            />
-        )
-    }
     function renderSeparator() {
         return (
           <View
@@ -70,6 +58,14 @@ export default function SearchUser(props) {
 
     return (
         <View style={{ flex: 1 }}>
+            <SearchBar
+                placeholder="Type Here..."
+                lightTheme
+                round
+                onChangeText={text => setValueInput(text)}
+                autoCorrect={false}
+                value={valueInput}
+            />
             <FlatList
                 data={patients}
                 renderItem={({ item }) => (
@@ -80,7 +76,6 @@ export default function SearchUser(props) {
                 )}
                 keyExtractor={item => item.id}
                 ItemSeparatorComponent={renderSeparator}
-                ListHeaderComponent={SearchHeader}
             />
         </View>
     );
