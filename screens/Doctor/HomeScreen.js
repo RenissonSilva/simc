bcf7b726873598e05a0cf12f4966f9e9f74b2c70
gmatheus66 <gmatheus66@hotmail.com>
2020-04-28 18:54:37 -0300

    Passando params para a tela de procura do usuario

--- screens/Doctor/HomeScreen.js
+++ screens/Doctor/HomeScreen.js
@@ -19,7 +19,7 @@ export default class DoctorHome extends React.Component {
         <View>
             <Text>DoctorHome</Text>
             <TouchableOpacity
-              onPress={ () => this.props.navigation.navigate('SearchUser')}
+              onPress={ () => this.props.navigation.navigate('SearchUser',{nextPage: 'CreateHandBook'})}
             >
               <Icon
                 name="add"
