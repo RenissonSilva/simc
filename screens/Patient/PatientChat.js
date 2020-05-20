355823ae560d6a6cc1e222f8767d7d6a2f1006e4
gmatheus66 <gmatheus66@hotmail.com>
2020-05-20 12:05:19 -0300

    Mudando titulo da aba da pagina do chat

--- screens/Patient/PatientChat.js
+++ screens/Patient/PatientChat.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class ChatScreen extends Component {
+  static navigationOptions = {
+    title: 'Chat',
+  };
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
