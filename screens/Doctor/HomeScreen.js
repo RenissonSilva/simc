0e9d54c3613d246fa3415ca126945cf21920d2dc
gmatheus66 <gmatheus66@hotmail.com>
2020-05-27 12:59:20 -0300

    Add estilo ao card  do prontuario

--- screens/Doctor/HomeScreen.js
+++ screens/Doctor/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import ListHandbook from '../HandBook/ListHandbook';
 
 export default class DoctorHome extends React.Component {
 
@@ -18,6 +19,9 @@ export default class DoctorHome extends React.Component {
     return (
         <View>
             <Text>DoctorHome</Text>
+            <View style={{marginLeft: '10%', marginRight: '10%', marginTop: '2%', marginBottom: '3%'}} >
+              <ListHandbook/>
+            </View>
             <TouchableOpacity
               onPress={ () => this.props.navigation.navigate('SearchUser',{nextPage: 'CreateHandBook'})}
             > 
