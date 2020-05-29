050a3a5d3ed3e0487e742004ac7c01acd2cb4c7a
gmatheus66 <gmatheus66@hotmail.com>
2020-05-29 14:20:17 -0300

    Estilo usado nos componentes do doutor

---
+++ screens/Doctor/style.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+import ListHandbook from '../HandBook/ListHandbook';
+
+export default StyleSheet.create({
+    iconaddhandbook:{
+        backgroundColor: '#FF5F54',
+        position:'absolute',
+        width: 60,
+        height: 65,
+        marginLeft:'2%',
+        borderRadius: 40,
+        top: 175,
+        left: 285,
+        bottom: 20,
+    },
+    iconaddhandbookinto:{
+        color: '#fff'
+    },
+    listHandbook:{
+        marginLeft: '10%',
+        marginRight: '10%',
+        marginTop: '2%',
+        marginBottom: '3%',
+    }
+});
