f014ab5185dd126e0c90b498a1065af2a4ffaf9f
Renisson Silva <renissonx@gmail.com>
2020-07-14 19:43:45 -0300

    Estilizando botão de logout

--- screens/HomeComponent/style.js
+++ screens/HomeComponent/style.js
@@ -1,202 +1,213 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-      btnsignout:{
-        marginTop: '3%',
-        marginBottom: '3%',
-        marginLeft: '35%',
-        marginRight: '35%',
-        borderRadius: 15,
-        backgroundColor: '#FF5F54', 
-      },
-      viewbtnsignout:{
-        marginTop: '2%',
-        marginBottom: '2%',
-        justifyContent: 'center',
-        flex: 1,
-        flexDirection: 'row'
-      },
-      textbtnsugnout:{
-        color: '#fff',
-        fontSize: 16,
-      },
-      iconbtnsiginout:{
-        color: '#fff',
-      },
-      cardlisthandbook:{
-        marginTop: '3%',
-        marginLeft: '15%'
-      },
-      grafico:{
-        marginTop: '3%',
-        marginLeft: '8%'
-      },
-      fundo: {
-        backgroundColor: '#ffffff'
-      },
-      container: {
-        flex: 1,
-      },
-      teste: {
-        flexDirection: 'row',
-      },
-      spinnerTextStyle: {
-        color: '#FFF'
-      },
-      btn: {
-        justifyContent: 'center',
-        width: '75%',
-        height: 110,
-        backgroundColor: '#fff',
-        margin: 15,
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#FF5F54',
-        alignSelf: 'center',
-      },
-      btnText: {
-        marginLeft: 30,
-        color: '#FF5F54',
-        fontSize: 16,
-      },
-      btnBpm: {
-        marginLeft: 50,
-        color: '#FF5F54',
-        fontSize: 40,
-        fontWeight: 'bold',
-      },
-      icon: {
-        alignSelf: 'center',
-        marginRight: '15%',
-        alignSelf: 'flex-end',
-        position: 'absolute',
-      },
-      monitora: {
-        color: '#8B8B8B',
-        fontSize: 16,
-        alignSelf: 'center',
-      },
-      btnB: {
-        color: '#FF5F54',
-        fontSize: 21,
-        alignSelf: 'flex-end',
-        marginLeft: 5,
-      },
-      imgProfile: {
-        borderRadius: 80,
-        width: 160,
-        height: 160,
-        alignSelf: 'center',
-        marginTop: 40,
-        borderColor: '#FF5F54',
-        borderWidth: 2,
-      },
-      nome: {
-        fontSize: 30,
-        color: '#FF5F54',
-        alignSelf: 'center',
-        fontWeight: 'bold',
-      },
-      ano: {
-        fontSize: 28,
-        color: '#FF5F54',
-        alignSelf: 'center',
-      },
-      info: {
-        marginTop: 10,
-        paddingBottom: 10,
-        fontSize: 22,
-        color: '#FF5F54',
-        alignSelf: 'center',
-        fontWeight: 'bold',
-      },
-      dado: {
-        fontSize: 20,
-        color: '#FF5F54',
-      },
-      imgIcon: {
-        width: 30,
-        height: 30,
-        margin: 10,
-      },
-      infoIcon: {
-        flexDirection: 'row',
-      },
-      containerDados: {
-        alignSelf: 'center',
-      },
-      //Relative
-      title: {
-        marginTop: 20,
-        marginLeft: 40,
-        paddingBottom: 10,
-        fontSize: 22,
-        color: '#FF5F54',
-        fontWeight: 'bold',
-      },
-      msgEmpty: {
-        marginBottom: 50,
-        fontSize: 19,
-        color: '#FF5F54',
-        fontWeight: 'bold',
-        alignSelf: 'center',
-      },
-      sadIcon: {
-        alignSelf: 'center',
-        marginTop: 50,
-        marginBottom: 10,
-        width: 60,
-        height: 60,
-      },
-      boxFamiliar: {
-        backgroundColor: '#FF5F54',
-        width: '80%',
-        height: 100,
-        borderRadius: 20,
-        alignSelf: 'center',
-        flexDirection: 'row',
-      },
-      boxFamiliarText: {
-        marginLeft: 10,
-        width: '60%',
-      },
-      imgProfileRelative: {
-        borderRadius: 40,
-        borderColor: 'white',
-        borderWidth: 2,
-        marginLeft: 20,
-        marginTop: 10,
-        width: 80,
-        height: 80,
-      },
-      msg: {
-        marginTop: 15,
-        fontSize: 19,
-        color: 'white',
-        fontWeight: 'bold',
-        alignSelf: 'center',
-      },
-      check: {
-        backgroundColor: 'white',
-        width: 90,
-        height: 35,
-        marginTop: 15,
-        borderRadius: 10,
-        justifyContent: 'center',
-      },
-      buttons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      },
-      imgAccept: {
-        width: 28,
-        height: 28,
-        alignSelf: 'center',
-      },
-      imgDenied: {
-        width: 20,
-        height: 20,
-        alignSelf: 'center',
-      },
-
+  btnsignout: {
+    marginTop: '3%',
+    marginBottom: '3%',
+    marginLeft: '35%',
+    marginRight: '35%',
+    borderRadius: 15,
+    backgroundColor: '#FF5F54',
+  },
+  viewbtnsignout: {
+    marginTop: '2%',
+    marginBottom: '2%',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  textbtnsugnout: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  iconbtnsiginout: {
+    color: '#fff',
+  },
+  cardlisthandbook: {
+    marginTop: '3%',
+    marginLeft: '15%',
+  },
+  grafico: {
+    marginTop: '3%',
+    marginLeft: '8%',
+  },
+  fundo: {
+    backgroundColor: '#ffffff',
+  },
+  container: {
+    flex: 1,
+  },
+  teste: {
+    flexDirection: 'row',
+  },
+  spinnerTextStyle: {
+    color: '#FFF',
+  },
+  btn: {
+    justifyContent: 'center',
+    width: '75%',
+    height: 110,
+    backgroundColor: '#fff',
+    margin: 15,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#FF5F54',
+    alignSelf: 'center',
+  },
+  btnText: {
+    marginLeft: 30,
+    color: '#FF5F54',
+    fontSize: 16,
+  },
+  btnBpm: {
+    marginLeft: 50,
+    color: '#FF5F54',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  icon: {
+    alignSelf: 'center',
+    marginRight: '15%',
+    alignSelf: 'flex-end',
+    position: 'absolute',
+  },
+  monitora: {
+    color: '#8B8B8B',
+    fontSize: 16,
+    alignSelf: 'center',
+  },
+  btnB: {
+    color: '#FF5F54',
+    fontSize: 21,
+    alignSelf: 'flex-end',
+    marginLeft: 5,
+  },
+  imgProfile: {
+    borderRadius: 80,
+    width: 160,
+    height: 160,
+    alignSelf: 'center',
+    marginTop: 40,
+    borderColor: '#FF5F54',
+    borderWidth: 2,
+  },
+  nome: {
+    fontSize: 30,
+    color: '#FF5F54',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  ano: {
+    fontSize: 28,
+    color: '#FF5F54',
+    alignSelf: 'center',
+  },
+  info: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 22,
+    color: '#FF5F54',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  dado: {
+    fontSize: 20,
+    color: '#FF5F54',
+  },
+  imgIcon: {
+    width: 30,
+    height: 30,
+    margin: 10,
+  },
+  infoIcon: {
+    flexDirection: 'row',
+  },
+  containerDados: {
+    alignSelf: 'center',
+  },
+  //Relative
+  title: {
+    marginTop: 20,
+    marginLeft: 40,
+    paddingBottom: 10,
+    fontSize: 22,
+    color: '#FF5F54',
+    fontWeight: 'bold',
+  },
+  msgEmpty: {
+    marginBottom: 50,
+    fontSize: 19,
+    color: '#FF5F54',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  sadIcon: {
+    alignSelf: 'center',
+    marginTop: 50,
+    marginBottom: 10,
+    width: 60,
+    height: 60,
+  },
+  boxFamiliar: {
+    backgroundColor: '#FF5F54',
+    width: '80%',
+    height: 100,
+    borderRadius: 20,
+    alignSelf: 'center',
+    flexDirection: 'row',
+  },
+  boxFamiliarText: {
+    marginLeft: 10,
+    width: '60%',
+  },
+  imgProfileRelative: {
+    borderRadius: 40,
+    borderColor: 'white',
+    borderWidth: 2,
+    marginLeft: 20,
+    marginTop: 10,
+    width: 80,
+    height: 80,
+  },
+  msg: {
+    marginTop: 15,
+    fontSize: 19,
+    color: 'white',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  check: {
+    backgroundColor: 'white',
+    width: 90,
+    height: 35,
+    marginTop: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imgAccept: {
+    width: 28,
+    height: 28,
+    alignSelf: 'center',
+  },
+  imgDenied: {
+    width: 20,
+    height: 20,
+    alignSelf: 'center',
+  },
+  submitText: {
+    fontSize: 20,
+    backgroundColor: '#FF5F54',
+    color: '#fff',
+    width: 150,
+    height: 50,
+    alignSelf: 'center',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderRadius: 10,
+    margin: 10,
+  },
 });
