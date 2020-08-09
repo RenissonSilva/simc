0de174d6bd6958010b157c8f220504f61c142721
Renisson Silva <renissonx@gmail.com>
2020-08-09 20:05:00 -0300

    Adicionando botão de sair no admin

--- screens/Admin/HomeAdmin.js
+++ screens/Admin/HomeAdmin.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { StyleSheet, Image, TouchableOpacity, Text, View, ImageBackground } from 'react-native';
+import IconLogout from '../../icon/logout.svg';
+import axios from 'axios';
+import http from '../../services/axiosconf';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class App extends React.Component {
   
+  signOut = async () => {
+    this.setState({loading: true});
+
+    await AsyncStorage.multiGet(['Token', 'User']).then(evt => {
+      this.setState({token: evt[0][1]});
+      this.setState({user: evt[1][1]});
+    });
+
+    await http
+      .post(
+        '/' + this.state.user + '/logout',
+        {},
+        {
+          headers: {
+            Accept: 'application/json',
+            Authorization: this.state.token,
+          },
+        },
+      )
+      .then(res => {
+        if (res.data) {
+          AsyncStorage.clear();
+          if (this.state.user == 'patient') {
+            GoogleFit.disconnect();
+          }
+          this.props.navigation.navigate('LoadHome');
+        }
+      })
+      .catch(error => {
+        console.log('error signout', error);
+        if (error) {
+          this.props.navigation.navigate('LoadHome');
+        }
+      });
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -16,6 +55,12 @@ export default class App extends React.Component {
         <TouchableOpacity style={styles.btn} onPress = {() => { this.props.navigation.navigate('')}}>
         <Icon name="clinic-medical" size={50} color="#fff" style={styles.icon}/>
           <Text style={styles.btnText}>Cadastrar instituição</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.signOut} style={styles.btnsignout}>
+          <View style={styles.viewbtnsignout}>
+            <IconLogout style={styles.iconbtnsiginout} />
+            <Text style={styles.textbtnsugnout}> Sair </Text>
+          </View>
         </TouchableOpacity>
       </View>
     );
@@ -69,5 +114,28 @@ const styles = StyleSheet.create({
     alignSelf:'center',
     marginRight:'10%',
     marginLeft:'5%',
-  }
+  },
+  btnsignout: {
+    marginTop: '5%',
+    marginBottom: '5%',
+    marginLeft: '35%',
+    marginRight: '35%',
+    borderRadius: 15,
+    padding: 5,
+    backgroundColor: '#FF5F54',
+  },
+  viewbtnsignout: {
+    marginTop: '2%',
+    marginBottom: '2%',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  textbtnsugnout: {
+    color: '#fff',
+    fontSize: 16,
+    alignSelf:'center',
+  },
+  iconbtnsiginout: {
+    color: '#fff',
+  },
 });
